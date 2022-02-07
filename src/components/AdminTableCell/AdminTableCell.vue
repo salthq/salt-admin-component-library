1a8a93bd644facf7aa10d19ae79630abeb15413e
Jason Tame <jason@advantagelearn.com>
2022-02-07 17:51:46 +0200

    Add admin table cell component

---
+++ src/components/AdminTableCell/AdminTableCell.vue
@@ -0,0 +1,11 @@
+<template>
+  <td class="px-6 py-4 text-sm leading-5 text-gray-600">
+    <slot />
+  </td>
+</template>
+
+<script lang="ts">
+export default {
+  name: "SltAdminTableCell",
+};
+</script>
