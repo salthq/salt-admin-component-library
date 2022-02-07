2d5ea4d70771fa0cd7e2e73f905782028ebc6374
Jason Tame <jason@advantagelearn.com>
2022-02-07 17:37:49 +0200

    Add better types for the arrays in admin table state

--- src/state/adminTableState.ts
+++ src/state/adminTableState.ts
@@ -1,14 +1,18 @@
 import { reactive } from "vue";
 
+type SelectedUsersType = Array<number>;
+type AllRowsIdsType = Array<number>;
+type SelectedRowsType = Array<number>;
+
 export default reactive({
   activeSortColumn: "id",
   activeSortState: "asc",
   activeQuery: "",
   adminTableData: {},
   allUsersSelected: false,
-  selectedUsers: [],
+  selectedUsers: [] as SelectedUsersType,
   allRowsSelected: false,
-  allRowsIDs: [],
-  selectedRows: [],
+  allRowsIDs: [] as AllRowsIdsType,
+  selectedRows: [] as SelectedRowsType,
   tablePerPageOption: 10,
 });
