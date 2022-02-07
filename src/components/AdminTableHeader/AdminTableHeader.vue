f0134e1fecd7c805a8f7f864a00e13c93deff56d
Jason Tame <jason@advantagelearn.com>
2022-02-07 16:39:00 +0200

    Create admin table header component

---
+++ src/components/AdminTableHeader/AdminTableHeader.vue
@@ -0,0 +1,66 @@
+<template>
+  <th
+    class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 text-gray-600 uppercase tracking-wider"
+    @click="$emit('click')"
+    :class="{ 'cursor-pointer hover:bg-gray-100': sort }"
+  >
+    <div class="flex items-center">
+      <div class="inline-block pr-3" v-if="sort">
+        <span><icon-sort-up :fill="iconSortUpFill" /></span>
+        <span><icon-sort-down :fill="iconSortDownFill" /></span>
+      </div>
+      <span v-if="label">{{ label }}</span>
+      <span v-else><slot /></span>
+    </div>
+  </th>
+</template>
+
+<script lang="ts">
+import { computed, defineComponent } from "vue";
+
+import state from "../../state/adminTableState";
+
+import IconSortUp from "../components/IconsSortUp/IconSortUp.vue";
+import IconSortDown from "@/Components/IconsSortDown/IconSortDown.vue";
+
+export default defineComponent({
+  name: "SltAdminTableHeader",
+  props: {
+    label: {
+      type: String,
+      required: false,
+    },
+    sort: {
+      type: String,
+      required: false,
+    },
+  },
+  components: { IconSortUp, IconSortDown },
+  setup(props) {
+    const activeSortFill = computed(() => {
+      return getComputedStyle(document.documentElement).getPropertyValue(
+        "--primary-700"
+      );
+    });
+
+    const iconSortUpFill = computed(() => {
+      return state.activeSortState === "desc" &&
+        state.activeSortColumn === props.sort
+        ? activeSortFill
+        : "#CFD3E1";
+    });
+
+    const iconSortDownFill = computed(() => {
+      return state.activeSortState === "asc" &&
+        state.activeSortColumn === props.sort
+        ? activeSortFill
+        : "#CFD3E1";
+    });
+
+    return {
+      iconSortUpFill,
+      iconSortDownFill,
+    };
+  },
+});
+</script>
