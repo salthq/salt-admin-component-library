09a05ec7456b98062baac0076d0b52d7c5c30150
Jason Tame <jason@advantagelearn.com>
2022-02-15 17:00:59 +0200

    Update app inertia link to use script setup

--- src/components/AppInertiaLink.vue
+++ src/components/AppInertiaLink.vue
@@ -4,65 +4,28 @@
   </Link>
 </template>
 
-<script lang="ts">
-import { computed, defineComponent } from "vue";
+<script setup lang="ts">
+import { computed } from "vue";
 
 import { Link } from "@inertiajs/inertia-vue3";
 
-export default defineComponent({
-  props: {
-    data: {
-      type: Object,
-      required: false,
-    },
-    href: {
-      type: String,
-      required: true,
-    },
-    method: {
-      type: String,
-      default: "get",
-    },
-    replace: {
-      type: Boolean,
-      default: false,
-    },
-    preserveScroll: {
-      type: Boolean,
-      default: false,
-    },
-    preserveState: {
-      type: Boolean,
-      default: null,
-    },
-    only: {
-      type: Array,
-      default: () => [],
-    },
-    headers: {
-      type: Object,
-      default: () => ({}),
-    },
-    newTab: {
-      type: Boolean,
-      default: false,
-    },
-    link: {
-      type: String,
-      default: null,
-    },
-  },
-  components: {
-    Link,
-  },
-  setup(props) {
-    const inertiaAs = computed(() => {
-      return props.method.toLowerCase() !== "get" ? "button" : "a";
-    });
+const props = withDefaults(
+  defineProps<{
+    data?: Record<string, unknown>;
+    headers?: Record<string, unknown>;
+    href: string;
+    link?: string;
+    method?: string;
+    newTab?: boolean;
+    only?: Array<any>;
+    preserveScroll?: boolean;
+    preserveState?: boolean;
+    replace?: boolean;
+  }>(),
+  { headers: () => ({}), method: "get", only: () => [] }
+);
 
-    return {
-      inertiaAs,
-    };
-  },
+const inertiaAs = computed(() => {
+  return props.method.toLowerCase() !== "get" ? "button" : "a";
 });
 </script>
