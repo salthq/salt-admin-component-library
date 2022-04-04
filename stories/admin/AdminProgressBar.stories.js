088856071f37b489a401b747132551b074e97a53
Jason Tame <jason@advantagelearn.com>
2022-04-04 14:50:24 +0200

    Create stories for admin progress bar

---
+++ stories/admin/AdminProgressBar.stories.js
@@ -0,0 +1,46 @@
+import SltAdminProgressBar from "../../src/components/AdminProgressBar/AdminProgressBar.vue";
+
+export default {
+  title: "Admin/Progress Bar",
+  component: SltAdminProgressBar,
+  argTypes: {
+    current: {
+      control: {
+        type: "number",
+      },
+      defaultValue: 0,
+    },
+    message: {
+      control: {
+        type: "text",
+      },
+    },
+    total: {
+      control: {
+        type: "number",
+      },
+      defaultValue: 100,
+    },
+  },
+};
+
+const Template = (args) => ({
+  components: { SltAdminProgressBar },
+  setup() {
+    return { args };
+  },
+  template: `<slt-admin-progress-bar v-bind="args" />`,
+});
+
+export const Default = Template.bind({});
+Default.args = {
+  current: 0,
+  total: 100,
+};
+
+export const InProgress = Template.bind({});
+InProgress.args = {
+  current: Math.floor(Math.random() * 99),
+  message: "Importing...",
+  total: 100,
+};
